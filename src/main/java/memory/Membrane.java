2904d1097fb5c09f753ce4675cf190e17b1da2e9
Carl Shiles <brenan@bot.shakti.local>
2018-01-28 17:10:05 -0600

    actually added membrance & note classes

---
+++ src/main/java/memory/Membrane.java
@@ -0,0 +1,24 @@
+package memory;
+
+/**
+ * This class serves as the interface between the data retrevial code and the end user code such as a terminal or web app.
+ */
+public class Membrane{
+
+    String createNote(){
+	return null;
+    }
+
+    void deleteNote(String id){
+
+    }
+
+    void modifyNote(String id){
+
+    }
+
+    Note getNote(String id){
+	return null;
+    }
+		    
+}
---
+++ src/main/java/memory/Note.java
@@ -0,0 +1,13 @@
+package memory;
+
+import java.time.*;
+
+public class Note{
+    
+    LocalDate start, end;
+    
+    public Note(LocalDate s, LocalDate e){
+	start=s;
+	end=e;
+    }
+}
